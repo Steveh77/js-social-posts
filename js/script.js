a87a6e53d11942f4c448736af7863ae4df15cbf7
Steveh77 <stederosa76@gmail.com>
2022-07-06 16:00:36 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -27,11 +27,11 @@
 // # Milestone 1
 const arrayPost = [
     {   id : 1 , 
-        nomeAutore : "Phil Mangione", 
+        nomeAutore : "Pieroh Mangione", 
         fotoAutore : "https://unsplash.it/300/300?image=15",
         data : "03-06-2022",    
         testo : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sintvoluptate recusandae architecto. Et nihil ullam aut alias.",
-        immaginePost : "https://unsplash.it/600/300?image=171",
+        immaginePost : "https://unsplash.it/600/300?image=174",
         likes : 80,
     },
     {   id : 2 , 
@@ -47,8 +47,8 @@ const arrayPost = [
         fotoAutore : "https://unsplash.it/300/300?image=15",
         data : "03-06-2022",    
         testo : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sintvoluptate recusandae architecto. Et nihil ullam aut alias.",
-        immaginePost : "https://unsplash.it/600/300?image=171",
-        likes : 99,
+        immaginePost : "https://unsplash.it/600/300?image=175",
+        likes : 9999,
     }
 
 ]
@@ -60,7 +60,45 @@ console.log(arrayPost)
 // Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 
 
+const container = document.getElementById('container')
+let post = '';
 
+for (let i = 0; i < arrayPost.length; i++) {
+    const element = arrayPost[i];
+    // console.log(element)
+    post += `<div class="post">
+    <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img class="profile-pic" src="${element['fotoAutore']}" />
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${element['nomeAutore']}</div>
+          <div class="post-meta__time">${element['data']}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">
+    ${element['testo']}
+    </div>
+    <div class="post__image">
+      <img src="${element['immaginePost']}" alt="" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <a class="like-button js-like-button" href="#" data-postid="1">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+            <span class="like-button__label">Mi Piace</span>
+          </a>
+        </div>
+        <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${element['likes']}</b> persone</div>
+      </div>
+    </div>
+  </div>`
+}
+console.log(post)
+container.innerHTML = post
 
 
 
